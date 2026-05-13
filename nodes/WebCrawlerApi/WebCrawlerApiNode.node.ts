5c78b7a7aa9a98f58e399f0b0df4b1a4b8da478d
AndeyM <andrew@webcrawlerapi.com>
2026-05-13 22:58:51 +0200

    Fix formatting

--- nodes/WebCrawlerApi/WebCrawlerApiNode.node.ts
+++ nodes/WebCrawlerApi/WebCrawlerApiNode.node.ts
@@ -60,13 +60,17 @@ export class WebCrawlerApiNode implements INodeType {
 
 				const body: Record<string, any> = { url, output_formats: [output_format] };
 
-				const response = await this.helpers.httpRequestWithAuthentication.call(this, 'webCrawlerApi', {
-					method: 'POST',
-					url: 'https://api.webcrawlerapi.com/v2/scrape',
-					headers: { 'Content-Type': 'application/json' },
-					body,
-					json: true,
-				});
+				const response = await this.helpers.httpRequestWithAuthentication.call(
+					this,
+					'webCrawlerApi',
+					{
+						method: 'POST',
+						url: 'https://api.webcrawlerapi.com/v2/scrape',
+						headers: { 'Content-Type': 'application/json' },
+						body,
+						json: true,
+					},
+				);
 
 				if (!response.success) {
 					throw new NodeOperationError(
